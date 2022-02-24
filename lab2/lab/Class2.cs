7c8a37e8b17a51912e2166343fb40913ef906ad9
Victerst <78637179+Victerst@users.noreply.github.com>
2022-02-24 23:07:53 +0700

    add file

---
+++ lab2/lab/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Class1
+{
+    class RomanNumberException : Exception
+    {
+        public RomanNumberException(string message)
+          : base(message)
+        {
+
+        }
+    }
+}
